ca38d935b2a39b5d0bd24ad660cb5fb448b8d6e5
RyanOkerlund <ryanokerlund13@gmail.com>
2019-09-06 22:36:40 -0600

    Working on Aim Slider Mechanic

---
+++ High-Noon-Game/Assets/Scripts/Crosshair.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    private bool isFirePressed;
+
+    private void Start()
+    {
+        this.GetComponent<BoxCollider2D>().enabled = false;
+        isFirePressed = false;
+    }
+
+    private void OnValidate()
+    {
+        this.GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(this.transform.parent.parent.GetComponent<PlayerController>().fireButton))
+        {
+            //crosshairAnim.SetBool("isFirePressed", true);
+            this.GetComponent<Animator>().enabled = false;
+            isFirePressed = true;
+            //aimSliderCrosshair.AddComponent<Rigidbody2D>();
+            this.GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isFirePressed)
+        {
+            if (collision.tag == "GreenZone")
+                Debug.Log("In the Green Zone!");
+            else if (collision.tag == "OrangeZone")
+                Debug.Log("In the Orange Zone!");
+            else if (collision.tag == "RedZone")
+                Debug.Log("In the Red Zone!"); 
+        }
+    }
+}
---
+++ High-Noon-Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public KeyCode fireButton;
+
+    //private GameObject aimSlider;
+
+    //private GameObject aimSliderCrosshair;
+    //private Animator crosshairAnim;
+
+    //void Start()
+    //{
+    //    aimSlider = this.transform.GetChild(0).gameObject;
+
+    //    aimSliderCrosshair = aimSlider.transform.GetChild(0).gameObject;
+    //    crosshairAnim = aimSliderCrosshair.GetComponent<Animator>();
+    //}
+
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(fireButton))
+    //    {
+    //        //crosshairAnim.SetBool("isFirePressed", true);
+    //        crosshairAnim.enabled = false;
+    //        //aimSliderCrosshair.AddComponent<Rigidbody2D>();
+    //        aimSliderCrosshair.AddComponent<BoxCollider2D>();
+    //    }
+    //}
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.tag == "GreenZone")
+    //        Debug.Log("In the Green Zone!");
+    //    else if (collision.tag == "OrangeZone")
+    //        Debug.Log("In the Orange Zone!");
+    //    else if (collision.tag == "RedZone")
+    //        Debug.Log("In the Red Zone!");
+    //}
+}
